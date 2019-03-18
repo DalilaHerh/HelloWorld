8a96da3c62b94d294878ef5a139b7c9848613843
FirstProject <dalila.herh@gmail.com>
2019-03-18 09:38:46 +0100

    First project.

---
+++ src/sample/Controller.java
@@ -0,0 +1,14 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+import java.awt.*;
+
+public class Controller {
+    @FXML private Label labelica;
+    public void HelloWorld(ActionEvent actionEvent) {
+        labelica.setText("Hello World");
+    }
+}
